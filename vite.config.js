e67987c7342947b35beff4db7538f8c02ec63780
jsantander-cl <j.santanderph@gmail.com>
2026-05-11 09:58:14 -0400

    porfafunciona

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
 
+export default defineConfig({
+  // Aquí va tu configuración
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        // Si borraste HOJA4, HOJA5 y HOJA6, asegúrate de que NO estén aquí
+      }
+    }
+  }
+})
