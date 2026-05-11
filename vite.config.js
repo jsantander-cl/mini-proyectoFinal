b90d244f9476fe6f1ae1d55964bf438d63c7a4ca
jsantander-cl <j.santanderph@gmail.com>
2026-05-11 09:33:38 -0400

    final final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
- import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        SOBREMI: resolve(__dirname, 'SOBREMI.html'),
+        HABILIDADES: resolve(__dirname, 'HABILIDADES.html'),
+        HOJA4: resolve(__dirname, 'HOJA4.html'),
+        HOJA5: resolve(__dirname, 'HOJA5.html'),
+        HOJA6: resolve(__dirname, 'HOJA6.html'),
+
+
+
+      },
+    },
+  },
+});
+
